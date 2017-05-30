9ac81913ace58eb2ea69cb32aec18ace1018d7d0
swalshe <stephaniewalshe1@gmail.com>
2017-05-30 19:45:13 +0100

    Brick Breaker Added.
    Signed-off-by: swalshe <stephaniewalshe1@gmail.com>

---
+++ Java/Brick_Breaker_Extended/Main.java
@@ -0,0 +1,16 @@
+package brickBreaker;
+import javax.swing.JFrame;
+public class Main {
+	public static void main(String[] args) {
+		JFrame obj=new JFrame();
+		Gameplay gameplay=new Gameplay();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("Brick Breaker");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
+	
+	}
+
+}
